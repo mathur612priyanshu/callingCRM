394dc2c4b7fa486b9edb467bcfa33f4e64191c95
Priyanshu <mathur612priyanshu@gmail.com>
2026-02-25 12:11:56 +0530

    http to https

--- admin-panel/src/config.js
+++ admin-panel/src/config.js
@@ -1,4 +1,4 @@
-let API_URL = "http://callingapi.trustingbrains.com/api";
+let API_URL = "https://callingapi.trustingbrains.com/api";
 // let API_URL = "http://localhost:5000/api"
 // export default API_URL;
 export default API_URL;
