c460285e9700a9262280b774f1c35f9f99aab181
Priyanshu <mathur612priyanshu@gmail.com>
2026-02-25 12:00:27 +0530

    domain in dashboard added

--- admin-panel/src/config.js
+++ admin-panel/src/config.js
@@ -1,4 +1,4 @@
-let API_URL = "http://72.62.197.59:5002/api";
+let API_URL = "http://callingapi.trustingbrains.com/api";
 // let API_URL = "http://localhost:5000/api"
 // export default API_URL;
 export default API_URL;
